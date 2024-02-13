9b0e1dff717a900d254c9c01c6eea4885ac65516
Edjere Evelyn <evelynedjere@gmail.com>
2024-02-13 22:56:49 +0100

    Remove unused import and component from index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SpeedInsights>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </SpeedInsights>
 );
