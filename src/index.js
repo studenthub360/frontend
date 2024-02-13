b7b8c3a061d70cb62024bbc1e061cfeaf6db141d
Edjere Evelyn <evelynedjere@gmail.com>
2024-02-13 22:55:44 +0100

    Add @vercel/speed-insights package

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <SpeedInsights>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </SpeedInsights>
 );
